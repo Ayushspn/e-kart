43b5058978b34f2f278c535004ca411ae7fcdcd2
ayushmishraims@gmail.com <ayushmishraims@gmail.com>
2020-01-08 14:09:29 +0530

    improving UI

--- src/Components/Sort/Price-Sort/Price-Sort.component.js
+++ src/Components/Sort/Price-Sort/Price-Sort.component.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Price-Sort.styles.scss';
 import {connect} from 'react-redux';
 import {sortItems} from '../../../redux/ShopList/ShopActionCreators';
 
 
 const PriceSort = ({sortShopItems}) => {
+    const [activeClass, setActiveClass] = useState('');
+
     return (
-        <div>
-            <button onClick = {() => sortShopItems('HIGH_TO_LOW')}> PRICE HIGH TO LOW</button>
-            <button onClick = {() => sortShopItems('LOW_TO_HIGH')}> PRICE LOW TO HIGH</button>
-            <button onClick = {() => sortShopItems('SORT_BY_DISCOUNT')}> DISCOUNT</button>
+        <div className ='price-sort-container'>
+            <button className ={`${activeClass === 'HIGH_TO_LOW'? 'active' : null} price-sort__button`} onClick = {() => sortShopItems('HIGH_TO_LOW'), () => setActiveClass('HIGH_TO_LOW')}> PRICE HIGH TO LOW</button>
+            <button className ={`${activeClass === 'LOW_TO_HIGH'? 'active' : null} price-sort__button`} onClick = {() => sortShopItems('LOW_TO_HIGH'), () => setActiveClass('LOW_TO_HIGH')}> PRICE LOW TO HIGH</button>
+            <button className ={`${activeClass === 'SORT_BY_DISCOUNT'? 'active' : null} price-sort__button`} onClick = {() => sortShopItems('SORT_BY_DISCOUNT'), () => setActiveClass('SORT_BY_DISCOUNT')}> DISCOUNT</button>
         </div>
     )
 }
--- src/Components/Sort/Sort.component.js
+++ src/Components/Sort/Sort.component.js
@@ -3,6 +3,7 @@ import './Sort.styles.scss'
 import PriceSort from './Price-Sort/Price-Sort.component';
 const Sort = () => {
     return <div className ='sort-container'>
+                <span><b>SORT BY</b> </span>
                 <PriceSort></PriceSort>
             </div>
 }
