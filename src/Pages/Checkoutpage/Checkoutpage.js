795550d5742e16196d8b8064f2db692643acb117
ayushmishraims@gmail.com <ayushmishraims@gmail.com>
2020-01-08 17:29:50 +0530

    improving UI accoring to wireframes

--- src/Pages/Checkoutpage/Checkoutpage.js
+++ src/Pages/Checkoutpage/Checkoutpage.js
@@ -8,9 +8,9 @@ const CheckOutPage = ({ cartItems, cartItemCount }) => {
 
     return (
         <div className='row chnge-width'>
-            <div className='col-lg-9 col-md-9 col-sm-12 col-xs-12' style={{'max-width' : '57rem', 'margin': 'auto'}}>
+            <div className='col-lg-9 col-md-9 col-sm-12 col-xs-12' style={{'maxWidth' : '57rem', 'margin': 'auto'}}>
                 <div className='row'>
-                    {cartItems && cartItems.length > 0 ? cartItems.map((cartItem) => (<ShoppingListItem id={cartItem.id} items={cartItem}></ShoppingListItem>))
+                    {cartItems && cartItems.length > 0 ? cartItems.map((cartItem) => (<ShoppingListItem key = {cartItem.id} id={cartItem.id} items={cartItem}></ShoppingListItem>))
                         : <div>Please add Item to Cart</div>
                     }
                 </div>
