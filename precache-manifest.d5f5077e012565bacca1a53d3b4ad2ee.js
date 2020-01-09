f038d27bf87c22700c5d9a4e38aa73318cda9ebb
ayushmishraims@gmail.com <ayushmishraims@gmail.com>
2020-01-09 11:01:17 +0530

    Updates

---
+++ precache-manifest.d5f5077e012565bacca1a53d3b4ad2ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46b1116cc6aeec6b0b86e8bdd927ed3",
+    "url": "/Ayushspn/e-kart/index.html"
+  },
+  {
+    "revision": "38bdf2361ffa3081f1fd",
+    "url": "/Ayushspn/e-kart/static/css/2.e21e6ff9.chunk.css"
+  },
+  {
+    "revision": "21cbbf45389b833c26c9",
+    "url": "/Ayushspn/e-kart/static/css/main.f00715f8.chunk.css"
+  },
+  {
+    "revision": "38bdf2361ffa3081f1fd",
+    "url": "/Ayushspn/e-kart/static/js/2.c701b605.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Ayushspn/e-kart/static/js/2.c701b605.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21cbbf45389b833c26c9",
+    "url": "/Ayushspn/e-kart/static/js/main.deaaee17.chunk.js"
+  },
+  {
+    "revision": "e52b2736afa640d0206c",
+    "url": "/Ayushspn/e-kart/static/js/runtime-main.dd2f45a3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ayushspn/e-kart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ayushspn/e-kart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ayushspn/e-kart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ayushspn/e-kart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ayushspn/e-kart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ayushspn/e-kart/static/media/logo.5d5d9eef.svg"
+  }
+]);
